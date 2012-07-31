21e78813c795c67c4ad5334cef632c4585d5d0f9
Mateusz Czerwiński <mtczerwinski@gmail.com>
2012-07-31 17:03:34 +0200

    replace gauges to google analytics

--- vendor/assets/javascripts/gauges.js
+++ vendor/assets/javascripts/gauges.js
@@ -1,11 +1,11 @@
-var _gauges = _gauges || [];
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33634491-1']);
+_gaq.push(['_setDomainName', 'dicteo.pl']);
+_gaq.push(['_trackPageview']);
+
 (function() {
-  var t   = document.createElement('script');
-  t.type  = 'text/javascript';
-  t.async = true;
-  t.id    = 'gauges-tracker';
-  t.setAttribute('data-site-id', '500d3946613f5d6f49000033');
-  t.src = '//secure.gaug.es/track.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(t, s);
-})();
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
