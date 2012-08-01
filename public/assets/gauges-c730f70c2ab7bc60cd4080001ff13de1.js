1c3bc52fca947aeb7735c6da3f8b253ab41e847e
Mateusz Czerwiński <mtczerwinski@gmail.com>
2012-08-01 11:27:26 +0200

    replace gauges to google analytics and recompile assets

---
+++ public/assets/gauges-c730f70c2ab7bc60cd4080001ff13de1.js
@@ -0,0 +1,1 @@
+var _gauges=_gauges||[];(function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.id="gauges-tracker",e.setAttribute("data-site-id","500d3946613f5d6f49000033"),e.src="//secure.gaug.es/track.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
